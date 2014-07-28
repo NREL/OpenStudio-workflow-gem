514ede4252ab80de7dec716c02d2d10686642e13
Nicholas Long <nicholas.long@nrel.gov>
2014-07-28 15:47:00 -0600

    add space character to snake case method

--- lib/openstudio-workflow.rb
+++ lib/openstudio-workflow.rb
@@ -50,7 +50,7 @@ class String
     gsub(/::/, '/').
         gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2').
         gsub(/([a-z\d])([A-Z])/, '\1_\2').
-        tr('-', '_').
+        tr(' -', '__').
         downcase
   end
 end

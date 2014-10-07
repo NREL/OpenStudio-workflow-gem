e1b434172622dc6194dcd13b8360617d402cba1a
Dan Macumber <dan.macumber@gmail.com>
2014-10-07 08:35:10 -0600

    Adding pat files

---
+++ spec/files/pat_project/export_pat_project.rb
@@ -0,0 +1,15 @@
+require 'openstudio'
+require 'fileutils'
+
+project = OpenStudio::AnalysisDriver::SimpleProject.open('E:\test\CloudTest').get
+zip = project.zipFileForCloud
+FileUtils.cp(zip.to_s, "project.zip")
+
+analysis = project.analysis
+
+data_points = analysis.dataPoints
+puts "data_points=#{data_points}"
+data_points.each do |data_point|
+  puts data_point
+  data_point.saveJSON("data_point_#{OpenStudio::removeBraces(data_point.uuid)}.json", true)
+end

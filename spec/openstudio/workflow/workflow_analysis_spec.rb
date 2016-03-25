bc30842ac3d48c73b6cae3982ac3e8cb82310b32
Dan Macumber <dan.macumber@gmail.com>
2016-03-25 11:21:13 -0600

    Adding schema validation test

--- spec/openstudio/workflow/workflow_analysis_spec.rb
+++ spec/openstudio/workflow/workflow_analysis_spec.rb
@@ -1,4 +1,53 @@
 require 'spec_helper'
+require 'json-schema'
+
+def get_schema(allow_additional_properties = true)
+  schema = nil
+  schema_path = File.dirname(__FILE__) + '/../../schema/osw.json'
+  expect(File.exists?(schema_path)).to be true
+  File.open(schema_path) do |f|
+    schema = JSON.parse(f.read, {:symbolize_names => true})
+  end
+  expect(schema).to_not be_nil
+  
+  schema[:definitions].each_value do |definition|
+    definition[:additionalProperties] = allow_additional_properties
+  end
+  
+  return schema
+end
+
+def get_osw(path)
+  osw = nil
+  osw_path = File.dirname(__FILE__) + '/../../files/' + path
+  expect(File.exists?(osw_path)).to be true
+  File.open(osw_path) do |f|
+    osw = JSON.parse(f.read, {:symbolize_names => true})
+  end
+  expect(osw).to_not be_nil
+  
+  return osw
+end
+
+def validate_osw(path, allow_additional_properties = true)
+  schema = get_schema(allow_additional_properties)
+  osw = get_osw(path)
+
+  errors = JSON::Validator.fully_validate(schema, osw)
+  expect(errors.empty?).to eq(true), "OSW '#{path}' is not valid, #{errors}"
+end
+
+describe 'OSW Schema' do
+  it 'should be a valid OSW file' do
+    validate_osw('compact_osw/compact.osw', true)
+    validate_osw('extended_osw/example/workflows/extended.osw', true)
+  end
+  
+  it 'should be a strictly valid OSW file' do
+    validate_osw('compact_osw/compact.osw', false)
+    validate_osw('extended_osw/example/workflows/extended.osw', true)
+  end
+end
 
 describe 'OSW Integration' do
   it 'should run compact OSW file' do

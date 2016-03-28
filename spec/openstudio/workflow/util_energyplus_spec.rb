b3d0f159238c2e82f24f1fd564c60e05391938f1
Dan Macumber <dan.macumber@gmail.com>
2016-03-28 10:40:10 -0600

    Failing test

--- spec/openstudio/workflow/util_energyplus_spec.rb
+++ spec/openstudio/workflow/util_energyplus_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe 'EnergyPlus Module' do
 
   it 'should find EnergyPlus' do
-    OpenStudio::Workflow::Util::EnergyPlus::find_energyplus
+    # todo (rhorsey) - How can I test this method?  If I change it to self.find_energyplus in energyplus.rb then other methods break - DLM
+    OpenStudio::Workflow::Util::EnergyPlus.find_energyplus
   end
 
 end

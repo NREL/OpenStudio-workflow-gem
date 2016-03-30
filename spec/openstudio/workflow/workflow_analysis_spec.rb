14eb0603d1367b6b1a85ecee96e8959c9c907efc
Dan Macumber <dan.macumber@gmail.com>
2016-03-30 12:32:30 -0600

    Adding failing unit test for ry

--- spec/openstudio/workflow/workflow_analysis_spec.rb
+++ spec/openstudio/workflow/workflow_analysis_spec.rb
@@ -48,7 +48,9 @@ describe 'OSW Schema' do
     validate_osw('extended_osw/example/workflows/extended.osw', true)
   end
 end
+
 describe 'OSW Integration' do
+
   it 'should run compact OSW file' do
     # for local, it uses the rundir as the uuid. When using the analysis gem, the root path is difficult because
     # it requires you to know the relative path to the measure which you already added when constructing the workflow.
@@ -66,12 +68,27 @@ describe 'OSW Integration' do
     expect(k).to be_instance_of OpenStudio::Workflow::Run
     expect(k.run).to eq :finished
   end
+  
   it 'should run an extended OSW file' do
     adapter_options = {
         workflow_filename: 'extended.osw'
     }
     adapter = OpenStudio::Workflow.load_adapter 'local', adapter_options
     relative_osw = File.join(__FILE__, './../../../files/extended_osw/example/workflows')
+    run_options = {
+        debug: true
+    }
+    k = OpenStudio::Workflow::Run.new adapter, relative_osw, run_options
+    expect(k).to be_instance_of OpenStudio::Workflow::Run
+    expect(k.run).to eq :finished
+  end
+  
+  it 'should run an alternate path OSW file' do
+    adapter_options = {
+        workflow_filename: 'in.osw'
+    }
+    adapter = OpenStudio::Workflow.load_adapter 'local', adapter_options
+    relative_osw = File.join(__FILE__, './../../../files/alternate_paths/osw_and_stuff')
     run_options = {
         debug: true
     }

fa07178a79179150ab18e7893a77b32aa9caa922
Dan Macumber <dan.macumber@gmail.com>
2016-04-19 19:14:27 -0600

    Adding test for case with empty seed and empty weather file

--- spec/openstudio/workflow/workflow_analysis_spec.rb
+++ spec/openstudio/workflow/workflow_analysis_spec.rb
@@ -2,6 +2,22 @@ require_relative './../../spec_helper'
 require 'json-schema'
 
 describe 'OSW Integration' do
+  it 'should run empty OSW file' do
+    adapter_options = {
+        workflow_filename: 'empty.osw',
+        output_directory: File.absolute_path(File.join(__FILE__, './../../../files/empty_seed_osw/run'))
+    }
+    input_adapter = OpenStudio::Workflow.load_input_adapter 'local', adapter_options
+    output_adapter = OpenStudio::Workflow.load_output_adapter 'local', adapter_options
+    relative_osw = File.join(__FILE__, './../../../files/empty_seed_osw')
+    run_options = {
+        debug: true
+    }
+    k = OpenStudio::Workflow::Run.new input_adapter, output_adapter, relative_osw, run_options
+    expect(k).to be_instance_of OpenStudio::Workflow::Run
+    expect(k.run).to eq :finished
+  end
+  
   it 'should run compact OSW file' do
     adapter_options = {
         workflow_filename: 'compact.osw',
